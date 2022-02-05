3e90dbd6a2121c8918784986e4221b07171f1339
SevJSix <iansevier04@gmail.com>
2022-02-05 13:45:26 -0700

    make tile entity per chunk a little better

--- src/main/java/me/sevj6/listeners/patches/TileEntityLimit.java
+++ src/main/java/me/sevj6/listeners/patches/TileEntityLimit.java
@@ -1,7 +1,11 @@
 package me.sevj6.listeners.patches;
 
 import me.sevj6.util.MessageUtil;
+import net.minecraft.server.v1_12_R1.BlockPosition;
+import net.minecraft.server.v1_12_R1.TileEntity;
+import org.bukkit.block.Block;
 import org.bukkit.craftbukkit.v1_12_R1.CraftChunk;
+import org.bukkit.craftbukkit.v1_12_R1.CraftWorld;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
@@ -12,8 +16,12 @@ public class TileEntityLimit implements Listener {
     public void onPlace(BlockPlaceEvent event) {
         net.minecraft.server.v1_12_R1.Chunk chunk = ((CraftChunk) event.getBlockPlaced().getChunk()).getHandle();
         if (chunk.getTileEntities().size() > 512) {
-            event.setCancelled(true);
-            MessageUtil.sendMessage(event.getPlayer(), "&3Too many tile entities in this chunk.");
+            Block block = event.getBlockPlaced();
+            TileEntity tileEntity = ((CraftWorld) block.getWorld()).getHandle().getTileEntity(new BlockPosition(block.getX(), block.getY(), block.getZ()));
+            if (tileEntity != null) {
+                event.setCancelled(true);
+                MessageUtil.sendMessage(event.getPlayer(), "&3Too many tile entities in this chunk.");
+            }
         }
     }
 }
