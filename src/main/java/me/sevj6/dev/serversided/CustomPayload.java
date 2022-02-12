ae8c064758895139367c28b38d93159757cd29ac
SevJSix <iansevier04@gmail.com>
2022-02-11 18:36:35 -0700

    make serverside auto32k AUTO PLACE INSTEAD OF MANUAL LMFAOOOOO

--- src/main/java/me/sevj6/dev/serversided/CustomPayload.java
+++ src/main/java/me/sevj6/dev/serversided/CustomPayload.java
@@ -5,12 +5,30 @@ import me.sevj6.event.NMSEventHandler;
 import me.sevj6.event.NMSPacketListener;
 import me.sevj6.event.events.PacketEvent;
 import net.minecraft.server.v1_12_R1.BlockPosition;
-import net.minecraft.server.v1_12_R1.PacketDataSerializer;
 import net.minecraft.server.v1_12_R1.PacketPlayInCustomPayload;
 import org.bukkit.Bukkit;
+import org.bukkit.Chunk;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
 
 public class CustomPayload implements NMSPacketListener {
     private final ServersidedAuto32k serversidedAuto32k = new ServersidedAuto32k();
+
+    private final int PLACE_RANGE = 6;
+
+    public static int countBlockPerChunk(Chunk chunk, Material material) {
+        int count = 0;
+        for (int x = 0; x < 16; ++x) {
+            for (int z = 0; z < 16; ++z) {
+                for (int y = 0; y < 256; ++y) {
+                    if (chunk.getBlock(x, y, z).getType() != material) continue;
+                    ++count;
+                }
+            }
+        }
+        return count;
+    }
 
     @NMSEventHandler
     public void onPacket(PacketEvent.Incoming event) {
@@ -18,11 +36,32 @@ public class CustomPayload implements NMSPacketListener {
             PacketPlayInCustomPayload packet = (PacketPlayInCustomPayload) event.getPacket();
             if (packet.a().equals("auto32k")) {
                 Bukkit.getScheduler().runTask(Impurity.getPlugin(), () -> {
-                    PacketDataSerializer serializer = packet.b();
-                    BlockPosition pos = serializer.e();
+                    BlockPosition pos = getAutoPlace32kPos(event.getPlayer());
                     serversidedAuto32k.doPlace(event.getPlayer(), pos);
                 });
             }
         }
+    }
+
+    private BlockPosition getAutoPlace32kPos(Player player) {
+        Location playerInitialLocation = player.getLocation();
+        for (int x = playerInitialLocation.getBlockX() - PLACE_RANGE; x <= playerInitialLocation.getBlockX() + PLACE_RANGE; x++) {
+            for (int y = playerInitialLocation.getBlockY() - PLACE_RANGE; y <= playerInitialLocation.getBlockY() + PLACE_RANGE; y++) {
+                for (int z = playerInitialLocation.getBlockZ() - PLACE_RANGE; z <= playerInitialLocation.getBlockZ() + PLACE_RANGE; z++) {
+                    Location location = new Location(playerInitialLocation.getWorld(), x, y, z);
+                    if (isInvalidPlaceLocation(location)) continue;
+                    return new BlockPosition(location.getBlockX(), location.getBlockY(), location.getBlockZ());
+                }
+            }
+        }
+        return null;
+    }
+
+    private boolean isInvalidPlaceLocation(Location location) {
+        Location obsidian = location.clone().add(0, 1, 0);
+        Location dispenser = obsidian.clone().add(0, 1, 0);
+        Location redstone = dispenser.clone().add(0, 1, 0);
+        return obsidian.getBlock().getType() != Material.AIR || dispenser.getBlock().getType() != Material.AIR || redstone.getBlock().getType() != Material.AIR
+                || !location.getNearbyPlayers(0.5).isEmpty();
     }
 }
