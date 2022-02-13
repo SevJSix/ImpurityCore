a9e0f50cf3e85a5a43ebcf84576763abd4dfd04f
SevJSix <iansevier04@gmail.com>
2022-02-13 11:05:50 -0700

    remove shitty patch

--- src/main/java/me/sevj6/util/PluginUtil.java
+++ src/main/java/me/sevj6/util/PluginUtil.java
@@ -115,7 +115,6 @@ public class PluginUtil extends Utils implements Data {
         bukkitListeners.add(new Meta113BedAura());
         bukkitListeners.add(new LavaDupe());
         bukkitListeners.add(new PlaytimeListeners(plugin));
-        bukkitListeners.add(new AntiAfkBedTrapKilling());
         bukkitListeners.add(new ArmorStandAiDisable());
         return bukkitListeners;
     }
